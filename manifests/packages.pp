b3743b239169d0afb31e98a2aee6bb676e195d7a
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-06 11:48:33 +0200

    initial packages
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ manifests/packages.pp
@@ -0,0 +1,17 @@
+class grafana::packages (version="1.8.1") {
+ 
+   exec {
+      'install_grafana':
+	command   => "wget http://grafanarel.s3.amazonaws.com/grafana-${version}.tar.gz && tar zxvf grafana-${version}.tar.gz",
+        cwd       => "/usr/share/",
+        unless    => "ls /usr/share/grafana-${version} 2>/dev/null", 
+   }
+
+   file {
+      '/usr/share/grafana':
+        ensure    => link,
+        target    => "/usr/share/grafana-${version}",
+        require   => Exec['install_grafana'],
+   }
+
+}
