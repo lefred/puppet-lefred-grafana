c0c140341fa1200812bacea6e19e9003516d69c6
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-06 11:56:15 +0200

    add path
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -4,6 +4,7 @@ class grafana::packages ($version="1.8.1") {
       'install_grafana':
 	command   => "wget http://grafanarel.s3.amazonaws.com/grafana-${version}.tar.gz && tar zxvf grafana-${version}.tar.gz",
         cwd       => "/usr/share/",
+        path      => "/bin/:/usr/bin/:/sbin/:/usr/sbin",
         unless    => "ls /usr/share/grafana-${version} 2>/dev/null", 
    }
 
