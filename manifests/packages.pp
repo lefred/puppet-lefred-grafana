ccae30a198c7179f83d9bdd8028513bc7cf629f0
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-20 09:57:48 +0200

    add packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -4,7 +4,7 @@ class grafana::packages {
  
    exec {
       'install_grafana':
-	command   => "wget http://grafanarel.s3.amazonaws.com/grafana-${version}.tar.gz && tar zxvf grafana-${version}.tar.gz && rm grafana-${version}.tar.gz",
+	command   => "curl http://grafanarel.s3.amazonaws.com/grafana-${version}.tar.gz | tar zx",
         cwd       => "/usr/share/",
         path      => "/bin/:/usr/bin/:/sbin/:/usr/sbin",
         unless    => "ls /usr/share/grafana-${version} 2>/dev/null", 
